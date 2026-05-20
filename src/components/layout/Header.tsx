17c300656f96267f1a6625a5b568ec13591de2be
Daviddlls <david.dlls05@gmail.com>
2026-05-19 23:25:33 -0500

    fix api url

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -555,7 +555,7 @@ export default function Header({ modulos, rol, nombreUsuario }: Props) {
                 </>
               ) : (
                 <Link
-                  href={resolveModuloRoute(m)}
+                  href={resolveModuloRoute(m)!}
                   onClick={() => setMobileMenuOpen(false)}
                   className="block px-3 py-2 rounded-lg hover:bg-[#0052cc] text-white transition"
                 >
